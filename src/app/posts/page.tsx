5cf8010df485ec9a3de72fd61b4ffec5ac9f7541
Yuuki Takagi <snow7777man@gmail.com>
2023-11-14 18:00:22 +0900

    create 1 file and update 1 file

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,27 @@
-import prisma from "../../../lib/prisma";
+// import prisma from "../../../lib/prisma";
+
+// const Page = async () => {
+//   const posts = await prisma.post.findMany();
+
+//   return (
+//     <div className="m-4">
+//       <h1 className="text-lg font-bold">記事一覧</h1>
+//       <ul>
+//         {posts.map((post) => (
+//           <li key={post.id}>{post.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Page;
+
+import { Post } from '@prisma/client';
 
 const Page = async () => {
-  const posts = await prisma.post.findMany();
+  const response = await fetch('http://localhost:3000/api/posts');
+  const posts: Post[] = await response.json();
 
   return (
     <div className="m-4">
